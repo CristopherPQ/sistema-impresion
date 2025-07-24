5803c2d7cf4c712769833e246d3f863b2a152b1f
Cristopher_PQ <cristopherpauccar2005@gmail.com>
2025-07-24 12:30:17 -0500

    Se agrego las funciones 'seleccionarTipo' y 'seleccionarPrioridad', ademas de peuq

--- Proyecto.cpp
+++ Proyecto.cpp
@@ -34,6 +34,8 @@ Documento* crearDocumento(string nombre, string tipo, Prioridad prioridad);
 void agregarAImpresos(Documento* documento);
 void agregarAEliminados(Documento* documento);
 void imprimirCola(Documento* documentos, int i);
+string seleccionarTipo();
+Prioridad seleccionarPrioridad();
 
 int main() {
     SetConsoleOutputCP (65001);
@@ -48,7 +50,7 @@ int main() {
 void agregarDocumento(string nombre, string tipo, Prioridad prioridad) {
     Documento* nuevo = crearDocumento(nombre, tipo, prioridad);
 
-    // Insertamos el documneto según su nivel de prioridad
+    // Insertamos el documento según su nivel de prioridad
     if (!cola || nuevo->prioridad < cola->prioridad) {
         nuevo->siguiente = cola;
         cola = nuevo;
@@ -61,12 +63,14 @@ void agregarDocumento(string nombre, string tipo, Prioridad prioridad) {
         nuevo->siguiente = actual->siguiente;
         actual->siguiente = nuevo;
     }
+
+    cout<<"-> Documento '"<<nuevo->nombre<<"' agregado a la cola con éxito."<<endl;
 }
 
 void eliminarDocumento(string nombre) {
     // Verificamos si la cola esta vacía
     if (!cola) {
-        cout<<"La cola está vacía."<<endl;
+        cout<<"-> La cola está vacía."<<endl;
         return;
     }
 
@@ -80,7 +84,7 @@ void eliminarDocumento(string nombre) {
 
     // Verificamos si hubo alguna coincidencia
     if (!actual->siguiente) {
-        cout<<"El documento no se encuentra en la cola."<<endl;
+        cout<<"-> El documento no se encuentra en la cola de impresión."<<endl;
         return;
     }
 
@@ -93,6 +97,8 @@ void eliminarDocumento(string nombre) {
     // Agregamos el documento a la cola de eliminados
     agregarAEliminados(actual);
 
+    cout<<"-> Documento '"<<actual->nombre<<"' eliminado exitosamente."<<endl;
+
     // Eliminamos el documento
     delete actual;
 }
@@ -100,11 +106,12 @@ void eliminarDocumento(string nombre) {
 void verColaDeImpresion() {
     // Verificamos si la cola esta vacía
     if (!cola) {
-        cout<<"La cola está vacía."<<endl;
+        cout<<"-> La cola de impresión está vacía."<<endl;
         return;
     }
 
     // Imprimimos la cola de impresión
+    cout<<"-> Cola de impresión actual: "<<endl;
     imprimirCola(cola);
 }
 
@@ -136,7 +143,7 @@ Documento* crearDocumento(string nombre, string tipo, Prioridad prioridad) {
 void imprimirCola(Documento* documento, int i = 1) {
     // Caso base de la recursión
     if (!documento) return;
-    
+
     cout<<i<<". "<<documento->nombre<<" "<<"["<<documento->tipo<<"]"<<" "<<"("<<static_cast<int>(documento->prioridad)<<")"<<endl;
     imprimirCola(documento->siguiente, ++i);
 }
@@ -149,4 +156,32 @@ void agregarAImpresos(Documento* documento) {
 void agregarAEliminados(Documento* documento) {
     documento->siguiente = eliminados;
     eliminados = documento;
+}
+
+string seleccionarTipo() {
+    // Arreglo con los tipos de documento
+    string arr[] = {"PDF", "DOCX", "JPEG"};
+
+    int opcion;
+    cout<<"-- Tipo de Documento --";
+    cout<<" 1. PDF\n 2. DOCX \n 3. JPEG\n";
+    cout<<"Seleccione una opción (1-3): ";
+    cin>>opcion;
+
+    // Retornamos el tipo de documento
+    return arr[opcion - 1];
+}
+
+Prioridad seleccionarPrioridad() {
+    // Arreglo con los niveles de prioridad
+    Prioridad arr[] = {Prioridad::Alta, Prioridad::Media, Prioridad::Baja};
+
+    int opcion;
+    cout<<"-- Prioridad del Documento --";
+    cout<<" 1. Alta\n 2. Media \n 3. Baja\n";
+    cout<<"Seleccione una opción (1-3): ";
+    cin>>opcion;
+
+    // Retornamos la prioridad del Documento
+    return arr[opcion - 1];
 }
